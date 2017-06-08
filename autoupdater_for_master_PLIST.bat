9f1b0007e4f2d3b07096b3444359aeb12010be46
Jack <radio@desktop>
2017-06-08 18:28:32 -0500

    added autoDL script for masterList

---
+++ autoupdater_for_master_PLIST.bat
@@ -0,0 +1,54 @@
+REM good sets
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvlfUn045gbeqsy_3l2x59Ge0
+
+REM harriet brown
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvlfsUQLAv-lLFrZwSJ3Qkb8w
+
+REM danksgiving 5
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvletVOqrTSV6Z2KQSTSbO0vc
+
+REM music for driving a rainsoaked, winding road
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvle35Srq1xdQpeLmB4fxFSud
+
+REM ulti dance plist
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvldTi4YYvsEIh0kegFHkVQAC
+
+REM night driving-city
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvleu-GCgjVdUJJTnR_Ut-HJk
+
+REM first mix
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvlcOE7wbmeRzAyWqFNBCkjRQ
+
+REM butt radio 
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvlcQcD1mKpGiTMfVl7es2jzb
+
+REM ulti deep house
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvldnS2LbHPXyqyCohzfwe5zy
+
+REM phatty bangers and mash
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvldfkRNdbBF5-tF20PbqZVxv
+
+REM ulti trap playlist
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PLYtsfBPanvlf3ZGgHF1JjrJBIsEiKvjbq
+
+REM dance moosiq
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0  https://www.youtube.com/playlist?list=PL766DC3438A36A450
+
+REM new denkest playlist
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvleBYeC1uqCOkbve2krSstsX
+
+REM glamhouse
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvldl6JSzG6xs2bgoBmhImbkj
+
+REM only music videos
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvlfj4fTK-KvuEycntYq8ijIZ
+
+REM night driving
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvleu-GCgjVdUJJTnR_Ut-HJk
+
+
+REM good streaming lobby music
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvlcvzQMQ9FiBYwFXdnWIbVxU
+
+REM chill elevator tunes
+youtube-dl -i --yes-playlist -wx --audio-format mp3 --audio-quality 0 https://www.youtube.com/playlist?list=PLYtsfBPanvlcxWAB3RAHdX-9xB1b3yA1R
